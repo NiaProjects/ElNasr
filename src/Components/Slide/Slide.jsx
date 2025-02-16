30561434abbbf6910232f41934cbca3255d18f67
NiaProjects <frontniamedia@gmail.com>
2025-02-16 13:50:35 +0200

    change image

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -13,7 +13,7 @@ export default function Slide() {
         <div className={`flex ${lang == "ar" ? "flex-row-reverse" : ""} `}>
 
                 <div className={` ${lang == "ar" ? "md:text-right" : "md:text-left"}  w-full md:w-1/2 text-center px-0 py-10  title flex  items-center justify-end  `}>
-                
+                {/* test img */}
                             <div className='w-full'>
                             <p className='text-sec text-2xl uppercase'>{t("slider1.title")}</p>
                                         <h1 className='text-6xl font-bold mt-2 uppercase'>{t("slider1.description")}</h1>
